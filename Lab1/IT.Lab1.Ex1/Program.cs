0d5bb9ecd2eff26b4d9dfac51af15f6dce902158
Andrey Savkin <77355577+Karacina@users.noreply.github.com>
2022-06-14 12:41:40 +0300

    Add files via upload

---
+++ Lab1/IT.Lab1.Ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Lab1.Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Buffer to hold a line as it is read in
+            string line;
+            // Loop until no more input (Ctrl-Z in a console, or end-of-file)
+            while ((line = Console.ReadLine()) != null)
+            {
+                // Format the data
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                // Write the results out to the console window
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Lab1/IT.Lab1.Ex3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace IT.Lab1.Ex2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            string line=testInput.Text;
+            line = line.Replace(",", " y:");
+            line = "x:" + line;
+            formattedText.Text = line;
+        }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Buffer to hold a line read from the file on standard input
+            string line;
+            // Loop until the end of the file
+            while ((line = Console.ReadLine()) != null)
+            {
+                // Format the data in the buffer
+                line = line.Replace(",", " y:");
+                line = "x:" + line + "\n";
+                // Put the results into the TextBlock
+                formattedText.Text += line;
+            }
+        }
+    }
+}
